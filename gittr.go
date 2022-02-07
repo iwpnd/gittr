564539f53d750163f61d7d31d91ef02726db0ab5
Benjamin Ramser <legionaerr@googlemail.com>
2022-02-07 20:17:25 +0100

    feat: add and test terminal and bearing

--- gittr.go
+++ gittr.go
@@ -1,8 +1,12 @@
 package gittr
 
 import (
+	"math"
+
 	geojson "github.com/paulmach/go.geojson"
 )
+
+const earthRadius = 6371008.8
 
 // Extent contains all 4 Positions of a bounding box
 type Extent struct {
@@ -25,6 +29,71 @@ type Feature struct {
 // FeatureCollection extends geojson.FeatureCollection
 type FeatureCollection struct {
 	geojson.FeatureCollection
+}
+
+func radToDegree(rad float64) float64 {
+	return rad * 180 / math.Pi
+}
+
+func degreeToRad(degree float64) float64 {
+	return degree * math.Pi / 180
+}
+
+func distanceToRadians(distance float64) float64 {
+	const r = earthRadius
+
+	return distance / r
+}
+
+// terminal calculates the terminal position travelling a distance
+// from a given origin
+// see https://www.movable-type.co.uk/scripts/latlong.html
+func terminal(start []float64, distance, bearing float64) []float64 {
+	latRad1 := degreeToRad(start[1])
+	lonRad1 := degreeToRad(start[0])
+
+	bearingRad := degreeToRad(bearing)
+	distanceRad := distanceToRadians(distance)
+
+	latRad2 := math.Asin(
+		math.Sin(latRad1)*
+			math.Cos(distanceRad) +
+			math.Cos(latRad1)*
+				math.Sin(distanceRad)*
+				math.Cos(bearingRad))
+
+	lonRad2 := lonRad1 + math.Atan2(
+		math.Sin(bearingRad)*
+			math.Sin(distanceRad)*
+			math.Cos(latRad1),
+		math.Cos(distanceRad)-
+			math.Sin(latRad1)*
+				math.Sin(latRad2))
+
+	// cap decimals at .00000001 degree ~= 1.11mm
+	lon := math.Round(radToDegree(lonRad2)*100000000) / 100000000
+	lat := math.Round(radToDegree(latRad2)*100000000) / 100000000
+
+	return []float64{lon, lat}
+}
+
+func bearing(start, end []float64) float64 {
+	lat1 := degreeToRad(start[1])
+	lat2 := degreeToRad(end[1])
+	lng1 := degreeToRad(start[0])
+	lng2 := degreeToRad(end[0])
+
+	a := math.Sin(lng2-lng1) * math.Cos(lat2)
+
+	b := (math.Cos(lat1)*
+		math.Sin(lat2) -
+		math.Sin(lat1)*
+			math.Cos(lat2)*
+			math.Cos(lng2-lng1))
+
+	o := math.Atan2(a, b)
+
+	return math.Mod((o*180/math.Pi + 360), 360.0)
 }
 
 // Extent creates bounding box for input Feature
--- gittr_test.go
+++ gittr_test.go
@@ -2,8 +2,21 @@ package gittr
 
 import (
 	"encoding/json"
+	"math"
 	"testing"
 )
+
+// test helper to appoximate coordinate equality
+func approxEqual(want, got, tolerance float64) bool {
+	diff := math.Abs(want - got)
+	mean := math.Abs(want+got) / 2
+
+	if math.IsNaN(diff / mean) {
+		return true
+	}
+
+	return (diff / mean) < tolerance
+}
 
 func TestExtent(t *testing.T) {
 	tests := []struct {
@@ -95,16 +108,111 @@ func TestExtent(t *testing.T) {
 		}
 
 		if got.w != test.expected.w {
-			t.Fatal("case:", test.tcase, "- W", got.w, "does not match expected ", test.expected.w)
+			t.Error("case:", test.tcase, "- W", got.w, "does not match expected ", test.expected.w)
 		}
 		if got.s != test.expected.s {
-			t.Fatal("case:", test.tcase, "- S", got.s, "does not match expected ", test.expected.s)
+			t.Error("case:", test.tcase, "- S", got.s, "does not match expected ", test.expected.s)
 		}
 		if got.e != test.expected.e {
-			t.Fatal("case:", test.tcase, "- E", got.e, "does not match expected ", test.expected.e)
+			t.Error("case:", test.tcase, "- E", got.e, "does not match expected ", test.expected.e)
 		}
 		if got.n != test.expected.n {
-			t.Fatal("case:", test.tcase, "- N", got.n, "does not match expected ", test.expected.n)
+			t.Error("case:", test.tcase, "- N", got.n, "does not match expected ", test.expected.n)
+		}
+	}
+}
+
+func TestTerminal(t *testing.T) {
+	test := []struct {
+		origin, expected  []float64
+		bearing, distance float64
+	}{
+		{
+			origin:   []float64{13.35, 52.45},
+			distance: 1112.758,
+			bearing:  90,
+			expected: []float64{13.3664, 52.45},
+		},
+		{
+			origin:   []float64{0.0, 0.0},
+			distance: 10000,
+			bearing:  180,
+			expected: []float64{0.0, -0.089932},
+		},
+		{
+			origin:   []float64{13.35, -52.45},
+			distance: 10000,
+			bearing:  180,
+			expected: []float64{13.35, -52.539932},
+		},
+	}
+
+	for _, test := range test {
+		got := terminal(test.origin, test.distance, test.bearing)
+		lon, lat := got[0], got[1]
+
+		if !approxEqual(test.expected[1], lat, 0.00001) {
+			t.Errorf("expected %+v, got: %+v", test.expected[1], lat)
+		}
+
+		if !approxEqual(test.expected[0], lon, 0.00001) {
+			t.Errorf("expected %+v, got: %+v", test.expected[0], lon)
+		}
+	}
+}
+
+func TestBearing(t *testing.T) {
+	test := []struct {
+		start, end []float64
+		expected   float64
+	}{
+		{
+			start:    []float64{0.5, 0.5},
+			end:      []float64{0.5, 0},
+			expected: 180.0,
+		},
+		{
+			start:    []float64{0.5, 0.5},
+			end:      []float64{0, 0},
+			expected: 225.0,
+		},
+		{
+			start:    []float64{0.5, 0.5},
+			end:      []float64{0, 0.5},
+			expected: 270.0,
+		},
+		{
+			start:    []float64{0.5, 0.5},
+			end:      []float64{0, 1},
+			expected: 315.0,
+		},
+		{
+			start:    []float64{0.5, 0.5},
+			end:      []float64{0.5, 1},
+			expected: 0.0,
+		},
+		{
+			start:    []float64{0.5, 0.5},
+			end:      []float64{1, 1},
+			expected: 45.0,
+		},
+		{
+			start:    []float64{0.5, 0.5},
+			end:      []float64{1, 0.5},
+			expected: 90.0,
+		},
+		{
+			start:    []float64{0.5, 0.5},
+			end:      []float64{1, 0},
+			expected: 135.0,
+		},
+	}
+
+	for _, test := range test {
+		got := bearing(test.start, test.end)
+
+		if !approxEqual(got, test.expected, 0.001) {
+			t.Errorf("expected %+v, got: %+v", got, test.expected)
 		}
 	}
 }
